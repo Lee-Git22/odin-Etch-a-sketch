dfe1ce9f8a4d88746dc9cf0e85e109c106c28158
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-07 11:29:43 -0500

    Implement color picker, reset button, and eraser

--- script.js
+++ script.js
@@ -23,22 +23,66 @@ for (i = 0; i < CONTAINER_SIZE; i += gridSize) {
 
 // TODO: Add a click and drag effect for inking sketch pad with cursor using event listeners
 let penToggle = false;
+let penColor = "black";
+let recentColor = "black";
 
+// Modifies the grid colors
 const grids = document.querySelectorAll(".grid");
 grids.forEach((grid) => {
-
+    
     // Toggles pen with click
     grid.addEventListener("mousedown", () => {
         penToggle = !penToggle;
-        grid.style.background = "black";        
+        grid.style.background = penColor;        
     });
-    
+
     // Change grid color on hover if toggle is active
     grid.addEventListener("mouseover", () => {
         if (penToggle) {
-            grid.style.background = "black";
+            grid.style.background = penColor;
         };
     });
 });
 
+// Features 
+
 // TODO: Add user input form to select how many grids to populate sketchpad (MAX 100)
+
+// TODO: Reset button
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    grids.forEach((grid) => {
+        grid.style.background = "white";
+    })
+})
+
+// TODO: Color Picker
+const colorPicker = document.querySelector("#colorpicker");
+colorPicker.addEventListener("change", () => {
+    penColor = colorPicker.value;
+    recentColor = penColor;
+})
+
+
+// TODO: Eraser & Pen
+const eraser = document.querySelector("#eraser");
+eraser.addEventListener("click", () => {
+    recentColor = penColor;
+    penColor = "white";
+});
+
+const pen = document.querySelector("#pen");
+pen.addEventListener("click", () => {
+    penColor = recentColor;
+})
+
+// TODO: Rainbow mode
+
+// TODO: Shade mode (adds increment amounts of shade)
+
+
+
+
+// Functions
+
+
